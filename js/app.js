a17e00956326f786b9e2dd10717de448e2866e2e
Mark Railton <marksrailton@gmail.com>
2016-01-26 09:49:16 +0000

    Adds routing and template files for remaining pages
    Updates links for some pages

--- js/app.js
+++ js/app.js
@@ -5,6 +5,18 @@ angular.module('schoolApp', ['ngRoute', 'schoolDirectives'])
         .when("/home", {
             templateUrl: "templates/home.html"
         })
+        .when("/news", {
+            templateUrl: "templates/news.html"
+        })
+        .when("/info/greenschools", {
+            templateUrl: "templates/info/greenschools.html"
+        })
+        .when("/info/booklists", {
+            templateUrl: "templates/info/booklists.html"
+        })
+        .when("/contact", {
+            templateUrl: "templates/contact.html",
+        })
         .otherwise({
             redirectTo: '/home'
         });
