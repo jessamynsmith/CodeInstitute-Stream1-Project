27618a0d8929c332cc80e9fccaa5537d40eb4908
Mark Railton <marksrailton@gmail.com>
2016-01-31 19:20:08 +0000

    Adds nl2br filter

--- js/app.js
+++ js/app.js
@@ -26,3 +26,11 @@ var app = angular.module('schoolApp', ['ngRoute','schoolControllers','schoolDire
             redirectTo: '/home'
         });
 })
+.filter('nl2br', function($sce){
+    return function(msg,is_xhtml) { 
+        var is_xhtml = is_xhtml || true;
+        var breakTag = (is_xhtml) ? '<br />' : '<br>';
+        var msg = (msg + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1'+ breakTag +'$2');
+        return $sce.trustAsHtml(msg);
+    }
+});
